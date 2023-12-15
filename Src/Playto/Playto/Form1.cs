ed592e6adffa62d8bfd8710bd80c76e2e8e33a71
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-12-15 13:48:43 +0100

    Fix a bug

--- Src/Playto/Playto/Form1.cs
+++ Src/Playto/Playto/Form1.cs
@@ -99,11 +99,9 @@ namespace Playto
             this.pictureBox1.Location = new Point(cx / 2 - this.pictureBox1.Size.Width / 2, cy * 1 / 4);
             this.progressBar1.Location = new Point(cx / 2 - this.progressBar1.Size.Width / 2, cy * 2 / 3);
             Task.Run(() => Loader());
-            CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--disable-gpu", "--disable-gpu-compositing");
+            CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--disable-web-security");
             CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
             await webView21.EnsureCoreWebView2Async(environment);
-            webView21.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.All);
-            webView21.CoreWebView2.WebResourceRequested += CoreWebView2_WebResourceRequested;
             webView21.CoreWebView2.Settings.AreDevToolsEnabled = true;
             webView21.CoreWebView2.ContainsFullScreenElementChanged += (obj, args) =>
             {
@@ -165,15 +163,6 @@ namespace Playto
                 this.Controls.Remove(label3);
                 this.Controls.Remove(pictureBox1);
             }
-        }
-        private void CoreWebView2_WebResourceRequested(object sender, CoreWebView2WebResourceRequestedEventArgs e)
-        {
-            CoreWebView2HttpRequestHeaders requestHeaders = e.Request.Headers;
-            requestHeaders.SetHeader("Access-Control-Max-Age", "0");
-            requestHeaders.SetHeader("Cache-Control", "max-age=0, public");
-            requestHeaders.SetHeader("Access-Control-Allow-Origin", "*");
-            requestHeaders.SetHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-            requestHeaders.SetHeader("Access-Control-Allow-Headers", "x-requested-with, content-type");
         }
         private void CoreWebView2_NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
         {
